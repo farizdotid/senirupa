9f4bc25f6bef4c6a8845a6f6c7efb1cbc0d2f16f
farizdotid <contact.farizramadhan@gmail.com>
2024-06-07 22:27:09 +0700

    feat: Blog layout, fix: Remove service navigation link

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -2,32 +2,12 @@
 const navBarLinks = [
   { name: "Home", url: "/" },
   { name: "Products", url: "/products" },
-  { name: "Services", url: "/services" },
   { name: "Gallery", url: "/galleries" },
   { name: "Events", url: "/events" },
   { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
-const footerLinks = [
-  {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
-    ],
-  },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
-];
 // An object of links for social icons
 const socialLinks = {
   facebook: "https://www.facebook.com/purwakartacreativehub/",
@@ -36,6 +16,5 @@ const socialLinks = {
 
 export default {
   navBarLinks,
-  footerLinks,
   socialLinks,
 };
