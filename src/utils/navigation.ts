a3a105c7fdef133d60f067f2a3bc799b69ac5c56
farizdotid <contact.farizramadhan@gmail.com>
2024-06-08 11:18:00 +0700

    fix: Fixes layout product

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,11 +1,9 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
-  { name: "Gallery", url: "/galleries" },
+  { name: "Galeri", url: "/products" },
   { name: "Events", url: "/events" },
   { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 // An object of links for social icons
